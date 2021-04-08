ee7c7a805d05e0c0c9a2b6cedfce279999204df4
Yash <31120678+yash-js@users.noreply.github.com>
2021-04-08 21:21:58 +0530

    Update fireabase.js

--- src/fireabase.js
+++ src/fireabase.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAv6JMf1BRYnao4hMHyVA_nQ2zYnKvrWO0",
-  authDomain: "linkedinclone-8e639.firebaseapp.com",
-  projectId: "linkedinclone-8e639",
-  storageBucket: "linkedinclone-8e639.appspot.com",
-  messagingSenderId: "235964730385",
-  appId: "1:235964730385:web:fd3c05423533af3327b5b0",
-  measurementId: "G-5ZGZV2PNGG",
+// YOU CAN GET CONFIG FROM FIREBASE
 };
 
 const app = firebase.initializeApp(firebaseConfig);
